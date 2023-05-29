0b787469a7297dd93aa681dd9dd2c760f8f343c2
laboratorio <laboratorio@UPOAULA10215>
2023-05-29 08:47:23 -0500

    Ejecucion 2 Botones Guardar y Borrar

---
+++ AR_NotePage.xaml.cs
@@ -0,0 +1,33 @@
+namespace AR_Apuntes;
+
+public partial class AR_NotePage : ContentPage
+{
+    string AR_fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+    public AR_NotePage()
+	{
+		InitializeComponent();
+
+        if (File.Exists(AR_fileName))
+            AR_TextEditor.Text = File.ReadAllText(AR_fileName);
+
+    }
+
+    private void AR_GuardarBoton_Clicked(object sender, EventArgs e) 
+    {
+        //Guarda el archivo
+        File.WriteAllText(AR_fileName, AR_TextEditor.Text);
+
+    }
+
+    private void AR_BorrarBoton_Clicked(object sender, EventArgs e)
+    {
+    
+        //Borra el archivo
+        if (File.Exists(AR_fileName))
+            File.Delete(AR_fileName);
+
+        AR_TextEditor.Text = string.Empty;
+    }
+
+}
