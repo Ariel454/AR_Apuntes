0e11884b3bebd9981cccea161bcb2c63faa7bf33
laboratorio <laboratorio@UPOAULA10215>
2023-05-29 08:21:09 -0500

    Primera Ejecución Correct

--- AR_AboutPage.xaml.cs
+++ AR_AboutPage.xaml.cs
@@ -6,4 +6,11 @@ public partial class AR_AboutPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void AR_LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
 }
+
